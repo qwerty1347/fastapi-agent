fd7541997f3f582c281094cc9ff36c9102b597fa
이건희 <qwertys1347@gmail.com>
2025-12-18 14:29:45 +0900

    chore: 패키지 및 config 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ALLOWED_ORIGINS: str | None
 
+    QDRANT_HOST: str
+
     STORAGE_PATH: str
     UPLOAD_PATH: str
     IMAGE_PATH: str
